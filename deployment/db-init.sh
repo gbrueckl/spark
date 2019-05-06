b8feb8fd4341efaf73c9491251985a9ec0b3cad3
Gerhard Brueckl <github@gbrueckl.at>
2019-05-06 17:31:26 +0200

    Update db-init.sh
    the original script was not valid and did not execute successfully and never called install-worker.sh
    this having issues executing this script on Azure Databricks v5.3

--- deployment/db-init.sh
+++ deployment/db-init.sh
@@ -15,7 +15,7 @@
 # https://github.com/dotnet/spark Releases section. For instance, for v0.2.0, you
 # would set it to the following URI:
 # https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
-DOTNET_SPARK_RELEASE=https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
+DOTNET_SPARK_GITHUB_RELEASE=https://github.com/dotnet/spark/releases/download/v0.2.0/Microsoft.Spark.Worker.netcoreapp2.1.linux-x64-0.2.0.tar.gz
 
 # No need to change this unless you choose to use a different location
 DBFS_INSTALLATION_ROOT=/dbfs/spark-dotnet
@@ -24,4 +24,4 @@ DOTNET_SPARK_WORKER_INSTALLATION_PATH=/usr/local/bin
 ###############################################################################
 
 set +e
-/bin/bash $DBFS_INSTALLATION_ROOT/$DOTNET_SPARK_RELEASE github $DBFS_INSTALLATION_ROOT/$DOTNET_SPARK_RELEASE $DOTNET_SPARK_WORKER_INSTALLATION_PATH
+/bin/bash $DBFS_INSTALLATION_ROOT/install-worker.sh github $DOTNET_SPARK_GITHUB_RELEASE $DOTNET_SPARK_WORKER_INSTALLATION_PATH
